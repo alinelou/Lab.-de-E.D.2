2106ffbb36b1c5eec6e7abcf46d36e3c7a4fd1ec
Aline Lourenço <104594619+alinelou@users.noreply.github.com>
2024-03-05 14:15:19 -0300

    Update 06-Questao.c

--- QUESTÕES/06-Questao.c
+++ QUESTÕES/06-Questao.c
@@ -3,27 +3,35 @@
 
 #include <stdio.h>
 
+struct ParInteiro {
+    int numero1;
+    int numero2;
+};
 
-int mdc(int a, int b) {
+int mdc(struct ParInteiro numeros) {
+    int a = numeros.numero1;
+    int b = numeros.numero2;
+
     if (b == 0) {
         return a;
     } else {
-        return mdc(b, a % b);
+        struct ParInteiro novoPar = {b, a % b};
+        return mdc(novoPar);
     }
 }
 
 int main() {
-    int num1, num2;
+    struct ParInteiro numeros;
 
-    
     printf("Digite o primeiro numero inteiro: ");
-    scanf("%d", &num1);
+    scanf("%d", &numeros.numero1);
 
     printf("Digite o segundo numero inteiro: ");
-    scanf("%d", &num2);
+    scanf("%d", &numeros.numero2);
 
-    int resultado = mdc(num1, num2);
-    printf("O Maximo Divisor Comum (MDC) de %d e %d é: %d\n", num1, num2, resultado);
+  
+    int resultado = mdc(numeros);
+    printf("O Maximo Divisor Comum (MDC) de %d e %d é: %d\n", numeros.numero1, numeros.numero2, resultado);
 
     return 0;
 }
